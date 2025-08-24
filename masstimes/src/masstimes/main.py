41eb7ad33cc61be92ccca231b50b1781d3d45895
Christian Paling <christian.paling@googlemail.com>
2025-08-24 12:38:01 +0200

    Add basic CI pipeline

--- masstimes/src/masstimes/main.py
+++ masstimes/src/masstimes/main.py
@@ -6,4 +6,5 @@ from .routers import home
 app = FastAPI(docs_url=None, redoc_url=None)
 
 app.mount("/static", StaticFiles(directory="src/masstimes/static"), name="static")
+
 app.include_router(home.router)
