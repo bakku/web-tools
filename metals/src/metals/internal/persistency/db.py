bd0ea0539d273195f70dd8a7d501b60c9f5699a9
Christian Paling <christian.paling@googlemail.com>
2025-10-29 00:14:53 +0100

    Change relative import to absolute one

--- metals/src/metals/internal/persistency/db.py
+++ metals/src/metals/internal/persistency/db.py
@@ -4,7 +4,7 @@ from collections.abc import Generator
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
-from ...routers.shared import is_development_mode
+from metals.routers.shared import is_development_mode
 
 engine = create_engine(
     os.getenv("DATABASE_URL", "sqlite:///db/database.db"), echo=is_development_mode()
